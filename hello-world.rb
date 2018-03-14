61523e22292eb238023d34b6ad22319329bbd87f
muneta <muneta@rolan.co.jp>
2018-03-14 17:02:11 +0900

    commit hello-world.rb

---
+++ hello-world.rb
@@ -0,0 +1,9 @@
+ class HelloWorld < Formula
+  desc "print hello-world"
+  homepage ""
+  url "https://github.com/muneta/hello-world/releases/download/v0.0.1/hello-world"
+  sha256 "561899571e5f266b97a56c6043a496df1c73c9eebf6f275a01624143baddf39c"
+   def install
+   bin.install
+    end
+end
