c1709025ffdc4d2291551d89ec540f632b609d05
muneta <muneta@rolan.co.jp>
2018-03-14 17:18:47 +0900

    add “hello-world”

--- hello-world.rb
+++ hello-world.rb
@@ -4,6 +4,6 @@
   url "https://github.com/muneta/hello-world/releases/download/v0.0.1/hello-world"
   sha256 "561899571e5f266b97a56c6043a496df1c73c9eebf6f275a01624143baddf39c"
    def install
-   bin.install
+   bin.install "hello-world"
     end
 end
